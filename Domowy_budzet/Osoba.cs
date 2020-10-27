4d66068546f4c9f4648c1e691ad3911d4dcf05bb
luffy120 <47242894+luffy120@users.noreply.github.com>
2020-10-27 10:23:15 +0100

    Dodaj pliki projektów.

---
+++ Domowy_budzet/Osoba.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domowy_budzet
+{
+    class Osoba
+    {
+        private int id_osoby;
+        private string imie;
+        private string nazwisko;
+        private bool isAdmin;
+
+
+        public void zaloguj()
+        {
+            
+        }
+        public void limit_wydatków()
+        {
+
+        }
+
+
+    }
+}
---
+++ Domowy_budzet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domowy_budzet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
